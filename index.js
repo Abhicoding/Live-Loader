45a52aabeb7a3fc70670389dfaa92e14b4d3c1cc
Abhicoding <shinganeabhishek@gmail.com>
2018-09-03 16:48:46 +0530

    wrote a basic watch read write program

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 // Write a basic file tree watcher
+const chokidar = require('chokidar')
+const fossilDelta = require('node-delta')
+const fs = require('fs')
+
+var target, origin, delta, result
+
+var watcher = chokidar.watch('watched', {ignored: /^\./, persistent: true});
+watcher
+.on('add', function(path) {console.log('File', path, 'has been added');})
+.on('change', function () {
+      console.log('change detected')
+      target = fs.readFileSync('watched/root.js')
+      if (!origin) {
+        origin = fs.readFileSync('write/root_clone.js')
+      }
+      delta = fossilDelta.create(origin, target)
+      result = fossilDelta.apply(origin, delta)
+      origin = result
+      fs.writeFile('write/root_clone.js', result, function (err) {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      })
+   })
+.on('unlink', function(path) {console.log('File', path, 'has been removed');})
+.on('error', function(error) {console.error('Error happened', error);})
+
 
 // Add a file delta emitter
 
